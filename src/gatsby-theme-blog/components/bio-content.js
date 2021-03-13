6b088e93dbc383825cf286ddf25c2b85c1864112
Androz2091 <androz2091@gmail.com>
2021-03-13 11:48:31 +0100

    :lipstick: Update bio

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,9 +1,10 @@
 import React from "react"
+import { Styled } from "theme-ui";
 
 export default function Bio() {
   return (
     <>
-      Welcome to my blog!
+      Hey! I'm <Styled.a href="https://github.com/Androz2091">Androz2091</Styled.a>, and this is my blog.
       {` `}
       <br />
       I sometimes find interesting things and write them here. I usually write in French but I might write in English in the future.
