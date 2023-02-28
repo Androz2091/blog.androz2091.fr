2285b9196c9e8c1904efa744800cb0eb6c8d5e47
Androz2091 <androz2091@gmail.com>
2023-02-28 14:18:36 +0100

    fix

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -7,7 +7,6 @@ export default function Bio() {
       Hey! I'm <Themed.a href="https://github.com/Androz2091">Androz2091</Themed.a>, and this is my blog.
       {` `}
       <br />
-      I sometimes find interesting things and write them here. I usually write in French but I might write in English in the future.
     </>
   )
 }
